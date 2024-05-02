acfd0c6fef61cb186811c28da2af0397e2fb5111
Barachiell <bernardpineiro@gmail.com>
2024-05-02 20:27:26 +0100

    Fixed Point in an Array

---
+++ Algorithm/may_2024/may_02_2024/FixedPoint.java
@@ -0,0 +1,23 @@
+/* 
+This problem was asked by Apple.
+
+A fixed point in an array is an element whose value is equal to its index. Given a sorted array of distinct elements, return a fixed point, if one exists. Otherwise, return False.
+
+For example, given [-6, 0, 2, 40], you should return 2. Given [1, 5, 7, 8], you should return False.
+ */
+
+public class FixedPoint {
+
+    public void checkFixedPoint(int[] arr){
+        int fixedPoint = IntStream.range(0, arr.length)
+                .filter(i -> arr[i] == i)
+                .findFirst()
+                .orElse(-1);
+
+        if(fixedPoint != -1){
+            System.out.println(fixedPoint);
+        } else{
+            System.out.println(false);
+        }
+    }
+}
